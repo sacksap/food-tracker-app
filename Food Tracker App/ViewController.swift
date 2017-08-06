47749b0f5f212446478d47f97d12e8bb157b96e0
Alexander Sacks <ASacks@alexanders-macbook-pro.local>
2017-08-06 13:45:34 -0700

    Added accessibility labels, values, and hints for VoiceOver.

--- Food Tracker App/ViewController.swift
+++ Food Tracker App/ViewController.swift
@@ -15,6 +15,7 @@ class ViewController: UIViewController, UITextFieldDelegate, UIImagePickerContro
     @IBOutlet weak var nameTextField: UITextField!
     @IBOutlet weak var mealNameLabel: UILabel!
     @IBOutlet weak var photoImageView: UIImageView!
+    @IBOutlet weak var ratingsControl: RatingsControl!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -76,9 +77,6 @@ class ViewController: UIViewController, UITextFieldDelegate, UIImagePickerContro
         imagePickerController.delegate = self
         
         present(imagePickerController, animated: true, completion: nil)
-    }
-    @IBAction func setDefaultLabelText(_ sender: UIButton) {
-        mealNameLabel.text = "Default Text"
     }
     
 
