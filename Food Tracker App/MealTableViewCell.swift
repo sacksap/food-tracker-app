faebe33b3df654d0c369b006a20b2f58798f9b2e
Alexander Sacks <ASacks@alexanders-macbook-pro.local>
2017-08-07 14:16:02 -0700

    Connected table to code. Now working to some extent.

---
+++ Food Tracker App/MealTableViewCell.swift
@@ -0,0 +1,29 @@
+//
+//  MealTableViewCell.swift
+//  Food Tracker App
+//
+//  Created by Alexander Sacks on 8/6/17.
+//  Copyright © 2017 Alexander Sacks. All rights reserved.
+//
+
+import UIKit
+
+class MealTableViewCell: UITableViewCell {
+    
+    // MARK: Properties
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var photoImageView: UIImageView!
+    @IBOutlet weak var ratingsControl: RatingsControl!
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
